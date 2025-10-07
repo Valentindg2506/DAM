9e659638a2c751819ddff5626bc765438a42f869
Valentin de Gennaro <valentindegennaro@gmail.com>
2025-10-07 12:08:11 +0200

    Create Listado clientes.py

---
+++ Ejercicios/Listado clientes.py
@@ -0,0 +1,23 @@
+class Cliente():
+	def __init__(self,nombre,apellidos,email,direccion):
+		self.nombre = nombre
+		self.apellido = apellidos
+		self.email = email
+		self.direccion = direccion
+
+clientes = []
+while True:
+
+	nombre = input("Introduce el nombre del cliente: ")
+	apellidos = input("Introduce los apellidos del cliente: ")
+	email = input("Introduce el email del cliente: ")
+	direccion = input("Introduce la direccion del cliente: ")
+
+	clientes.append(Cliente(nombre,apellidos,email,direccion))
+	
+	for cliente in clientes:
+		print("-----------------------")
+		print(cliente(nombre))
+		print(cliente(apellido))
+		print(cliente(email))
+		print(cliente(direccion))
