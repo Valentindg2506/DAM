643163fa61b1299c8100c2900068ad8b6b983b21
Valentin Antonio de Gennaro <valentindegennaro@gmail.com>
2025-10-29 10:09:34 +0100

    29/10

---
+++ Ejercicios/EJ - SUB/U2/002-EJ.py
@@ -0,0 +1,8 @@
+'''
+	
+	v0.1 Valentin Antonio De Gennaro
+	
+'''
+import math
+
+numero = sqrt(numero)
--- Ejercicios/EJ - SUB/U3/006-EJ.py
+++ Ejercicios/EJ - SUB/U3/006-EJ.py
@@ -1,18 +1,18 @@
 def raizSegura(numero):
-'''
-    Función que calcula la raíz cuadrada de un número dado.
-    Entradas: numero (int, float o str) que se espera que sea numérico
-    Salidas: resultado de la raíz cuadrada como float (o cero si hay fallo)
-    Capturas de error:
-      1.- Si es numérico
-      2.- Si se puede convertir a número
-      3.- Si no es un número positivo
-'''
-from math import sqrt
+	'''
+		Función que calcula la raíz cuadrada de un número dado.
+		Entradas: numero (int, float o str) que se espera que sea numérico
+		Salidas: resultado de la raíz cuadrada como float (o cero si hay fallo)
+		Capturas de error:
+		  1.- Si es numérico
+		  2.- Si se puede convertir a número
+		  3.- Si no es un número positivo
+	'''
+	from math import sqrt
 
 	if isinstance(numero, (int, float)):
 		if numero >= 0:
-	  		raiz = sqrt(numero)
+			raiz = sqrt(numero)
 			return raiz
 		else:
 			return 0
@@ -24,7 +24,9 @@ from math import sqrt
 			return entero_raiz
 		except:
 			return 0
-		
-	
+
+numero = int(input("Introduce el numero: "))
+
+print(raizSegura(numero))
 
 
