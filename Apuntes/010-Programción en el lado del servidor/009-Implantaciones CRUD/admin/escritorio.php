c7ab700ca6c1f73126423b8711894c8be97d046f
Valentindg2506 <valentindegennaro@gmail.com>
2025-12-11 11:10:00 +0100

    1

---
+++ Apuntes/010-Programción en el lado del servidor/009-Implantaciones CRUD/admin/escritorio.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<title>LocalHost - Panel de control</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/estilo.css">
+	</head>
+	<body>
+		<nav>
+			<button>Noticias</button>
+			<button>Autores</button>
+		</nav>
+		<main>
+			<?php include "inc/read/Leer.php"; ?>
+		</main>
+	</body>
+</html>
+
+
