cccc9336c88cd06c08f20b61d23f17cc90600fe2
Valentindg2506 <valentindegennaro@gmail.com>
2025-12-12 14:23:58 +0100

    1

--- Apuntes/010-Programción en el lado del servidor/009-Implantaciones CRUD/admin/escritorio.php
+++ Apuntes/010-Programción en el lado del servidor/009-Implantaciones CRUD/admin/escritorio.php
@@ -12,10 +12,18 @@
 		</nav>
 		<main>
 			<?php
+			// ESTO SE CONOCE COMO ROUTER (ENRUTADOR) ////////////////////////
 				if(isset($_GET['accion'])){
-					if($_GET['accion'] == "nuevo"){
-						include "inc/create/formulario.php";
+					if($_GET['accion'] == "nuevo"){									// DEFINO LA ACCION NUEVO
+						include "inc/create/formulario.php";						// INCLUYO FORMULARIO.PHP
+						
+					}else if($_GET['accion'] == "eliminar"){						// DEFINO LA ACCION ELIMINAR
+						include "inc/delete/eliminar.php";							// INCLUYO ELIMINAR.PHP
+						
+					}else if($_GET['accion'] == "editar"){							// DEFINO LA ACCION EDITAR
+						include "inc/update/formulario_actualizar.php";				// INCLUYO FORMULARIO_ACTUALIZAR.PHP
 					}
+					
 				}else{
 					include "inc/read/Leer.php";
 				}
--- Apuntes/010-Programción en el lado del servidor/009-Implantaciones CRUD/admin/inc/read/Leer.php
+++ Apuntes/010-Programción en el lado del servidor/009-Implantaciones CRUD/admin/inc/read/Leer.php
@@ -16,6 +16,8 @@
         echo "<td>".$fila['fecha_publicacion']."</td>";
         echo "<td>".$fila['autor_id']."</td>";
         echo "<td>".$fila['contenido']."</td>";
+        echo "<td><a href='?accion=editar&id=".$fila['id']."' class='editar' title='Cuidado que vas a editar un dato'>✏️</a></td>";
+        echo "<td><a href='?accion=eliminar&id=".$fila['id']."' class='eliminar' title='⚠️ Mas cuidado todavia que vas a eliminar un dato ⚠️'>❌️</a></td>";
       echo "</tr>";
     }
 
