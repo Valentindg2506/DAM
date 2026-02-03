250a1fb970edf25c1ed55651cdf87d640ddbed13
Valentín Antonio de Gennaro <valentindegennaro@gmail.com>
2026-02-03 13:03:34 +0100

    1

---
+++ Apuntes/010-Programción en el lado del servidor/016-Proyecto camaron/001-Inicio/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="es">
+	<head>
+		<title>Camaron Prop</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<header><h1>Camaron Prop</h1></header>
+		<nav>
+			<form action="?" method="POST">
+				<select>
+					<option>Seleccione una localidad...</option>
+					<option value="Valencia">Valencia</option>
+					<option value="Alboraya">Alboraya</option>
+					<option value="Torrent">Torrent</option>
+					<option value="Gandía">Gandía</option>
+					<option value="Sagunto">Sagunto</option>
+					<option value="Paterna">Paterna</option>
+					<option value="Burjassot">Burjassot</option>
+					<option value="Xàtiva">Xàtiva</option>
+					<option value="Cullera">Cullera</option>
+				</select>
+				<input type="submit">
+			</form>
+		</nav>
+		<main>
+			<section></section>
+		</main>
+		<footer>(c) 2026 No me puedo creer que este proyecto se llame Camarón</footer>
+	</body>
+</html>
